633f7da84dddbba714f59a5a803885c6fa65d848
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2024-12-16 12:07:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,10 @@
 N = int(input())
 
-for i in range(N // 5, -1, -1):
-    reminder = N - 5 * i
-    if reminder % 3 == 0:
-        j = reminder // 3
-        print(i + j)
+for i in range(N//5,  -1 , -1):
+    re = N - 5 * i
+    if re % 3 == 0:
+        j = re // 3
+        print(i+j)
         break
 else:
     print(-1)
